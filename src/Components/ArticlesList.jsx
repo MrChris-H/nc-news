3f3dbe99404de5a52894658949898eebcdcc4d7f
MrChris-H <christopher.hopkins998@gmail.com>
2022-03-08 16:29:57 +0000

    add loading for articles list

--- src/Components/ArticlesList.jsx
+++ src/Components/ArticlesList.jsx
@@ -6,12 +6,18 @@ import { useParams } from "react-router-dom";
 
 const ArticlesList = () => {
   const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   const { topic } = useParams();
   useEffect(() => {
+    setIsLoading(true);
     getArticles(topic).then(({ articles }) => {
       setArticles(articles);
+      setIsLoading(false);
     });
   }, [topic]);
+
+  if (isLoading) return <p>Loading ...</p>;
   return (
     <section id="section-articles-list">
       <NavMobile />
