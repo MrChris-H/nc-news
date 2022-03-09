a5a4e5330ef08d8849dfa40b6f3347c461fc2833
MrChris-H <christopher.hopkins998@gmail.com>
2022-03-09 11:10:06 +0000

    add avatar and user input

--- src/Components/CommentPost.jsx
+++ src/Components/CommentPost.jsx
@@ -1,8 +1,23 @@
 const CommentPost = () => {
   return (
-    <div>
-      <p>Comment Post</p>
-    </div>
+    <section id="section-comment-post">
+      <div className="comment-post">
+        <div className="comment-post-avatar">
+          <img
+            src="https://static-cdn.jtvnw.net/jtv_user_pictures/71b9faed-de95-4263-8c3f-70bf3dac28b6-profile_image-300x300.png"
+            className="comment-card-avatar-img"
+          ></img>
+        </div>
+        <div className="comment-post-content">
+          <input
+            type="text"
+            id="comment"
+            name="comment"
+            placeholder="Add a comment..."
+          />
+        </div>
+      </div>
+    </section>
   );
 };
 
