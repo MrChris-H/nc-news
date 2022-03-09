c81f2e1c07b2b0f7fcc1c453b190f11f8422d707
MrChris-H <christopher.hopkins998@gmail.com>
2022-03-09 12:55:34 +0000

    create user context for logged in user

--- src/App.js
+++ src/App.js
@@ -3,20 +3,30 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import ArticlesList from "./Components/ArticlesList";
 import ArticleByID from "./Components/ArticleById";
+import { UserContext } from "./context/UserContext";
+import { useState } from "react";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState({
+    username: "jessjelly",
+    avatar_url:
+      "https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141",
+    name: "jess",
+  });
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<ArticlesList />} />
-          <Route path="/articles" element={<ArticlesList />} />
-          <Route path="/articles/:topic" element={<ArticlesList />} />
-          <Route path="/article/:articleId" element={<ArticleByID />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <UserContext.Provider value={{ loggedIn, setLoggedIn }}>
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route path="/" element={<ArticlesList />} />
+            <Route path="/articles" element={<ArticlesList />} />
+            <Route path="/articles/:topic" element={<ArticlesList />} />
+            <Route path="/article/:articleId" element={<ArticleByID />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
