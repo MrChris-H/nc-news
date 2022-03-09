14a617b12b5d48390be113934ca1d0b5a6ec479b
MrChris-H <christopher.hopkins998@gmail.com>
2022-03-09 11:45:29 +0000

    add submit btn

--- src/Components/CommentPost.jsx
+++ src/Components/CommentPost.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
 const CommentPost = () => {
+  const [comment, setComment] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    setComment("");
+  };
+
   return (
-    <section id="section-comment-post">
-      <div className="comment-post">
+    <form id="form-comment-post" onSubmit={handleSubmit}>
+      <div className="comment-post-input">
         <div className="comment-post-avatar">
           <img
             src="https://static-cdn.jtvnw.net/jtv_user_pictures/71b9faed-de95-4263-8c3f-70bf3dac28b6-profile_image-300x300.png"
@@ -12,12 +22,16 @@ const CommentPost = () => {
           <input
             type="text"
             id="comment"
-            name="comment"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
             placeholder="Add a comment..."
           />
         </div>
       </div>
-    </section>
+      <div className="comment-post-submit">
+        <button type="submit">Comment</button>
+      </div>
+    </form>
   );
 };
 
