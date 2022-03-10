d0d4fb11576a990edf8015b10e0398d9a6271ad9
MrChris-H <christopher.hopkins998@gmail.com>
2022-03-10 12:27:55 +0000

    limit to 1 vote

--- src/Components/Vote.jsx
+++ src/Components/Vote.jsx
@@ -4,14 +4,15 @@ import ArticleByID from "./ArticleById";
 const Votes = ({ votes, apiUpdate, id }) => {
   const [displayVotes, setDisplayVotes] = useState(0);
 
-  useEffect(() => {
-    setDisplayVotes(votes);
-  }, []);
-
-  const handleVote = (increment) => {
-    setDisplayVotes(displayVotes + increment);
+  const handleVote = (increment, oldVotes) => {
+    if (increment === displayVotes) {
+      setDisplayVotes(0);
+      increment = -increment;
+    } else {
+      setDisplayVotes(increment);
+    }
     apiUpdate(id, increment).catch(() => {
-      setDisplayVotes(displayVotes - increment);
+      setDisplayVotes(oldVotes);
     });
   };
 
@@ -20,20 +21,20 @@ const Votes = ({ votes, apiUpdate, id }) => {
       <div
         className="votes-arrow-container-top"
         onClick={() => {
-          handleVote(1);
+          handleVote(1, displayVotes);
         }}
       >
-        <i className="votes-arrow arrow up"></i>
+        <button className="votes-arrow arrow up"></button>
       </div>
 
-      <span className="votes-number">{displayVotes}</span>
+      <span className="votes-number">{votes + displayVotes}</span>
       <div
         className="votes-arrow-container-bottom"
         onClick={() => {
-          handleVote(-1);
+          handleVote(-1, displayVotes);
         }}
       >
-        <i className="votes-arrow arrow down"></i>
+        <button className="votes-arrow arrow down"></button>
       </div>
     </>
   );
