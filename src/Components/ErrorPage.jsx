346537e92dd66c6dd7fccc7f566c906f82c3b86c
MrChris-H <christopher.hopkins998@gmail.com>
2022-03-11 10:25:20 +0000

    add reusable error component

---
+++ src/Components/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const ErrorPage = ({ status, msg }) => {
+  console.log(status, msg);
+  return (
+    <div>
+      <p>{`${status || 404}, ${msg || "Looks like you're lost"}`}</p>
+      <Link to="/">Back to Saftey</Link>
+    </div>
+  );
+};
+
+export default ErrorPage;
